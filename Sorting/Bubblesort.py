21d36ca50d46ae49325f50c2b150af51f2962995
Lakshmi-Jahnavi-Bavisetti <bljptp111@gmail.com>
2024-05-29 22:53:05 +0530

    sorting techniques

---
+++ Sorting/Bubblesort.py
@@ -0,0 +1,15 @@
+def performbubblesort(nums):
+    n=len(nums)
+    fixThisPosition=n-1
+    
+    while(fixThisPosition>0):
+        for index in range(0,fixThisPosition):
+            if nums[index]>nums[index+1]:
+                nums[index],nums[index+1]=nums[index+1],nums[index]
+        print(nums)
+        fixThisPosition-=1
+nums=[9,8,7,6,5,4,3,2,1]
+print(nums)
+performbubblesort(nums)
+print(nums)
+            
